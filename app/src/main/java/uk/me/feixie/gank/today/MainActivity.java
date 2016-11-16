30b49a89d34144657deb3bc3fc864af705112861
Fei Xie <fei@rolleragency.co.uk>
2016-11-16 23:59:37 +0000

    added first contract

--- app/src/main/java/uk/me/feixie/gank/today/MainActivity.java
+++ app/src/main/java/uk/me/feixie/gank/today/MainActivity.java
@@ -1,9 +1,6 @@
-package uk.me.feixie.gank;
+package uk.me.feixie.gank.today;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import uk.me.feixie.gank.R;
+import uk.me.feixie.gank.Utils.Constants;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -20,17 +20,9 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +32,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        TodayFragment todayFragment = (TodayFragment) getSupportFragmentManager().findFragmentByTag(Constants.FRAGMENT_TODAY);
+        if (todayFragment == null) {
+            todayFragment = TodayFragment.newInstance();
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_main, todayFragment).commit();
+
+        TodayPresenter todayPresenter = new TodayPresenter(todayFragment);
     }
 
     @Override
